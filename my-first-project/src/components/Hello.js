d1a54379e24d27055ddd789390cb4e4d37332acc
SymonMuchemi <simonkenya846@gmail.com>
2023-09-26 21:48:59 +0300

    ES6 Syntax

--- my-first-project/src/components/Hello.js
+++ my-first-project/src/components/Hello.js
@@ -1,7 +1,11 @@
-function Hello(){
-    return (
-        <h1>Hello world </h1>
-    )
-}
+// function Hello(){
+//     return (
+//         <h1>Hello world </h1>
+//     )
+// }
+
+// using ES6 Syntax
+
+const Hello = () => <h1>Hello React</h1>
 
 export default Hello
