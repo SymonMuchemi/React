230439f834a9e9dce6c499e8926326d877c00c58
SymonMuchemi <simonkenya846@gmail.com>
2023-09-27 16:31:28 +0300

    Setting advice count

--- my-first-project/src/App.js
+++ my-first-project/src/App.js
@@ -1,14 +1,27 @@
+import { useState } from "react";
+
 export default function App(){
-  async function getAdvice(){
-    const res = await fetch('https://api.adviceslip.com/advice');
-    const data = await res.json();
-    console.log(data);
+  
+    const [advice, setAdvice] = useState("");
+    const [count, setCount] = useState(0);
+
+    async function getAdvice(){    
+        const res = await fetch('https://api.adviceslip.com/advice');
+        const data = await res.json();
+        
+        setCount(function (count) {
+                return count + 1;
+            })
+        setAdvice(data.slip.advice);
   }
   
   return(
     <>
-      <h1>Hello world!</h1>
+      <h1>{ advice }</h1>
       <button onClick={ getAdvice }>Get Advice</button>
+      <p>
+        You have read <strong>{count}</strong> pieces of advice.
+      </p>
     </>
   )
 }
