3fc4c68df51302ad59d7065d0681b9012189c5bd
SymonMuchemi <simonkenya846@gmail.com>
2023-09-27 15:53:36 +0300

    Jonas course

--- my-first-project/src/App.js
+++ my-first-project/src/App.js
@@ -1,19 +1,1 @@
-import './App.css';
-import Message from './components/Message';
-import Profile from './components/Profile';
 
-function App() {
-  return (
-    <div className="App">
-        {/* <Profile name="Symon" lastName="Powell">
-            <h3>This is a personal profile</h3>
-        </Profile>
-        <Profile name="Jordan" lastName="Spark">
-            <h3>This is another profile</h3>
-        </Profile> */}
-        <Message messageContent="This is a message"/>
-    </div>
-  );
-}
-
-export default App;
