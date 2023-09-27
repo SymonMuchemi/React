27797fc316f0619013afd4711ae2c3a239ef3713
SymonMuchemi <simonkenya846@gmail.com>
2023-09-27 16:07:44 +0300

    Fetchng advice

--- my-first-project/src/App.js
+++ my-first-project/src/App.js
@@ -1,1 +1,14 @@
-
+export default function App(){
+  async function getAdvice(){
+    const res = await fetch('https://api.adviceslip.com/advice');
+    const data = await res.json();
+    console.log(data);
+  }
+  
+  return(
+    <>
+      <h1>Hello world!</h1>
+      <button onClick={ getAdvice }>Get Advice</button>
+    </>
+  )
+}
