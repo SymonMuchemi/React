21358608b891c2036dd186489804a2189b078266
SymonMuchemi <simonkenya846@gmail.com>
2023-09-29 01:07:31 +0300

    recreated pizzaData.js and eported data

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -1,67 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const pizzaData = [
-    {
-      name: "Focaccia",
-      ingredients: "Bread with italian olive oil and rosemary",
-      price: 6,
-      photoName: "pizzas/focaccia.jpg",
-      soldOut: false,
-    },
-    {
-      name: "Pizza Margherita",
-      ingredients: "Tomato and mozarella",
-      price: 10,
-      photoName: "pizzas/margherita.jpg",
-      soldOut: false,
-    },
-    {
-      name: "Pizza Spinaci",
-      ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
-      price: 12,
-      photoName: "pizzas/spinaci.jpg",
-      soldOut: false,
-    },
-    {
-      name: "Pizza Funghi",
-      ingredients: "Tomato, mozarella, mushrooms, and onion",
-      price: 12,
-      photoName: "pizzas/funghi.jpg",
-      soldOut: false,
-    },
-    {
-      name: "Pizza Salamino",
-      ingredients: "Tomato, mozarella, and pepperoni",
-      price: 15,
-      photoName: "pizzas/salamino.jpg",
-      soldOut: true,
-    },
-    {
-      name: "Pizza Prosciutto",
-      ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
-      price: 18,
-      photoName: "pizzas/prosciutto.jpg",
-      soldOut: false,
-    },
-  ];
-  
+import data from './pizzaData.js'
 
-function App(){
+function App() {
     return (
         <div>
             <h1>Hello React</h1>
-            <Pizza/>
+            <Pizza />
+            <Pizza />
         </div>
     )
 }
 
-function Pizza(){
-    console.log(pizzaData[2].name)
+function Pizza(props) {
+    console.log(data[2].name)
     return (
         <div>
-            <h2>{pizzaData[2].name}</h2>
-            <p>{pizzaData[2].ingredients}</p>
+            <img src='pizzas/spinaci.jpg' alt='pizza spinaci' />
+            <h2>{data[4].name}</h2>
+            <p>{data[2].ingredients}</p>
         </div>
     )
 }
@@ -70,6 +28,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
     <React.StrictMode>
-        <App/>
+        <App />
     </React.StrictMode>
 )
