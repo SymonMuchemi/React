cb807034b182dcea9308ad678495d53139f8d625
SymonMuchemi <simonkenya846@gmail.com>
2023-09-29 22:40:00 +0300

    skills

--- profile-card-challege/src/components/data.js
+++ profile-card-challege/src/components/data.js
@@ -1,6 +1,32 @@
 const myData = {
     name: "Simon Muchemi",
-    description: "I'm a fullstack web deveoper skilled in react JS and spring boot. When I'm not coding I love cycling, hiking, reading novels and watching movies."
+    description: "I'm a fullstack web deveoper skilled in react JS and spring boot. When I'm not coding I love cycling, hiking, reading novels and watching movies.",
+    skills: [
+        {
+            skill: "HTML + CSS",
+            color: "#2763EA"
+        },
+        {
+            skill: "JavaScript",
+            color: "#EED91D"
+        },
+        {
+            skill: "React",
+            color: "#60DAFB"
+        },
+        {
+            skill: "Java",
+            color: "#262639"
+        },
+        {
+            skill: "Spring Boot",
+            color: "#6CB43C"
+        },
+        {
+            skill: "Git + GitHub",
+            color: "#E84E32"
+        },
+    ]
 }
 
 export default myData;
---
+++ profile-card-challege/src/components/skills.js
@@ -0,0 +1,9 @@
+const Skillset = (props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Skillset;
