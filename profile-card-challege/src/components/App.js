9316e8c64843eac8e26d21c76375a4126a06bfbf
SymonMuchemi <simonkenya846@gmail.com>
2023-09-30 15:55:18 +0300

    jsx

--- profile-card-challege/src/components/App.js
+++ profile-card-challege/src/components/App.js
@@ -2,6 +2,7 @@ import Avatar from './avatar';
 import './index.css';
 import myData from './data';
 import Intro from './intro';
+import Skilllist from './skills';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <h1>Profile card challenge</h1>
       <Avatar/>
       <Intro description={myData.description}/>
+      <Skilllist/>
     </div>
   )
 }
--- profile-card-challege/src/components/skills.js
+++ profile-card-challege/src/components/skills.js
@@ -1,9 +1,30 @@
-const Skillset = (props) => {
+const Skill = (props) => {
+    const style = {
+        backgroundColor: `${props.Skillcolor}`
+    }
     return (
-        <div>
-            
+        <div className="skill" style={style}>
+            <p>{props.skillName}</p>
         </div>
+
     )
 }
 
-export default Skillset;
+const Skilllist = (props) => {
+    
+    return (
+        <>
+            <div className="skills">
+                <Skill skillName='HTML + CSS' Skillcolor='#2763EA'/>
+                <Skill skillName='JavaScript' Skillcolor='#EED91D'/>
+                <Skill skillName='React' Skillcolor='#60DAFB'/>
+                <Skill skillName='Java' Skillcolor='#262639'/>
+                <Skill skillName='Spring Boot' Skillcolor='#6CB43C'/>
+                <Skill skillName='Git + GitHub' Skillcolor='#E84E32'/>
+            </div>
+    
+        </>
+    )
+}
+
+export default Skilllist;
