ae23cb4efcd48cb479c35e06a58bac6635812406
SymonMuchemi <simonkenya846@gmail.com>
2023-10-03 15:17:04 +0300

    React fragment ---<React.Fragment key={""}>

--- pizza-menu/src/components/menu.js
+++ pizza-menu/src/components/menu.js
@@ -1,5 +1,6 @@
 import Pizza from './pizza';
 import data from '../pizzaData'
+import {Fragment} from "react";
 
 const Menu = () => {
     const pizzas = data;
@@ -9,7 +10,7 @@ const Menu = () => {
             <main className='menu'>
                 <h2>Our menu</h2>
                 { numPizzas > 0 ? (
-                    <>
+                    <React.Fragment key={'anything'}>
                         <p>
                             Authentic Italian cuisine. 6 creative dishes to choose from. All from our stone oven, all organic, all
                             delicious.
@@ -19,7 +20,7 @@ const Menu = () => {
                                 <Pizza pizzaObj={pizza} key={pizza.name}/>
                             ))}
                         </ul>
-                    </>
+                    </React.Fragment>
                     ) : <p>We're still working on our menu. Please come back later 🙃</p>
                 }
             </main>
